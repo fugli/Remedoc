307bfcb7c7f267b1865291c49486e3a3f074d96a
prince <kumarprince280695@gmail.com>
2015-06-22 01:10:50 +0530

    allcities of lyberate

--- Lyberate/city.py
+++ Lyberate/city.py
@@ -15,10 +15,13 @@ def fn (url):
 	html_page = urllib2.urlopen(url)
 	soup = BeautifulSoup(html_page)
 	return soup
-# url ='https://www.lybrate.com/city-index/'+str(num)
-soup = BeautifulSoup (open('srcpg.html'))
-print >>open('locality.txt','w'),''
-option = soup.find_all('div',{'class':'span3'})
-# print option
-for item in option:
-	print >>open('locality.txt','a'),item.a['href'].encode('ascii','ignore')
+
+for num in range(1,11):
+	url ='https://www.lybrate.com/city-index/'+str(num)
+	soup = fn(url)
+	# soup = BeautifulSoup (open('srcpg.html'))
+	print >>open('allcity.txt','w'),''
+	option = soup.find_all('div',{'class':'span3'})
+	# print option
+	for item in option:
+		print >>open('all.txt','a'),item.a['href'].encode('ascii','ignore')
